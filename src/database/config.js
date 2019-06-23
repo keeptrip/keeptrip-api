003fc21f9104f1c77a9c65abfbe2ed379df41ed9
Vyacheslav Kostenko <namielusi@gmail.com>
2019-06-24 03:09:33 +0400

    Add sequelize and first migration for it

---
+++ src/database/config.js
@@ -0,0 +1,15 @@
+const config = require('config')
+
+const { database: dbConfig } = config
+const env = process.env.NODE_ENV || 'development'
+
+module.exports = {
+  [env]: {
+    username: dbConfig.user,
+    password: dbConfig.pass,
+    database: dbConfig.name,
+    host: dbConfig.host,
+    port: dbConfig.port,
+    dialect: dbConfig.dialect
+  }
+}
---
+++ src/database/migrations/20190623224019-trips.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('trips', {
+      id: {
+        field: 'id',
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.fn('uuid_generate_v4'),
+      },
+      title: {
+        field: 'title',
+        type: Sequelize.STRING(255)
+      },
+      createdAt: {
+        field: 'created_at',
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        field: 'updated_at',
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+      },
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+   return queryInterface.dropTable('trips')
+  }
+};
