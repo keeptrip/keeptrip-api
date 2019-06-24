b1086cb2cdbb605637ce32ab7af33684bcfda150
Vyacheslav Kostenko <namielusi@gmail.com>
2019-06-24 04:08:20 +0400

    Install uuid-ossp inside first migration

--- src/database/migrations/20190623224019-trips.js
+++ src/database/migrations/20190623224019-trips.js
@@ -2,30 +2,32 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('trips', {
-      id: {
-        field: 'id',
-        type: Sequelize.UUID,
-        allowNull: false,
-        primaryKey: true,
-        defaultValue: Sequelize.fn('uuid_generate_v4'),
-      },
-      title: {
-        field: 'title',
-        type: Sequelize.STRING(255)
-      },
-      createdAt: {
-        field: 'created_at',
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.fn('now'),
-      },
-      updatedAt: {
-        field: 'updated_at',
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.fn('now'),
-      },
+    return queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";').then(function() {
+      return queryInterface.createTable('trips', {
+        id: {
+          field: 'id',
+          type: Sequelize.UUID,
+          allowNull: false,
+          primaryKey: true,
+          defaultValue: Sequelize.fn('uuid_generate_v4'),
+        },
+        title: {
+          field: 'title',
+          type: Sequelize.STRING(255)
+        },
+        createdAt: {
+          field: 'created_at',
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.fn('now'),
+        },
+        updatedAt: {
+          field: 'updated_at',
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.fn('now'),
+        },
+      })
     })
   },
 
