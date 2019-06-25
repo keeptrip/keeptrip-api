c7088eacc3c292f277414bc4f0896c8caa2d2956
Vyacheslav Kostenko <namielusi@gmail.com>
2019-06-25 07:21:04 +0400

    Add properties for descriptors support

--- babel.config.js
+++ babel.config.js
@@ -8,8 +8,9 @@ module.exports = {
         '@babel/typescript'
     ],
     plugins: [
-        '@babel/proposal-class-properties',
         '@babel/proposal-object-rest-spread',
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
         ['module-resolver', {
             root: ['./src', './test'],
             alias: {
