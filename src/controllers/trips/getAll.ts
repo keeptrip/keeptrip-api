fb1f5d3b2ab83a25bc17e3702e5e4d34b8618700
Vyacheslav Kostenko <namielusi@gmail.com>
2019-06-25 07:22:43 +0400

    Add simple /trips endpoint without options

--- src/controllers/trips/getAll.ts
+++ src/controllers/trips/getAll.ts
@@ -1,11 +1,22 @@
 import Hapi from '@hapi/hapi'
 
+import { Trip } from '@src/database/models/Trip'
+
 export const route: Hapi.ServerRoute = {
     method: 'get',
     path: '/trips',
-    handler(req, h): object {
+    async handler(req, h): Promise<object> {
+        const { rows, count } = await Trip.findAndCountAll()
+
         return {
-            message: 'OK',
+            total: count,
+            items: rows.map((item: Trip): object => ({
+                id: item.id,
+                title: item.title,
+                description: item.description,
+                createdAt: item.created_at,
+                updatedAt: item.updated_at
+            }))
         }
     },
 }
--- test/src/controllers/trips/getAll.spec.ts
+++ test/src/controllers/trips/getAll.spec.ts
@@ -1,17 +1,22 @@
 import chai, { assert } from 'chai'
 import { stub } from 'sinon'
 import sinonChai from 'sinon-chai'
+import jsonChai from 'chai-json-schema'
 import config from 'config'
 
 import { sequelize } from '@src/database'
+import { routes } from '@src/controllers'
 import { Server } from '@src/Server'
 
 import { truncateDatabase } from '@test/utils/database'
+import { ServerInjectResponse } from '@hapi/hapi';
 
 chai.use(sinonChai)
+chai.use(jsonChai)
 
-describe('Trips :: Get all', function (): void {
+describe('Trips routes', function (): void {
     const server = new Server(config.get('server.host'), Number(config.get('server.port')))
+    server.routes = routes
     
     beforeEach(async function (): Promise<void> {
         await server.prepare()
@@ -28,7 +33,9 @@ describe('Trips :: Get all', function (): void {
 
     describe('create', function (): void {})
 
-    describe('get', function (): void {
+    // ########################################### ###########################################
+    
+    describe('[GET] /trips', function (): void {
         beforeEach(async function (): Promise<void> {
             await sequelize.query(`
                 INSERT INTO trips (title, description) VALUES
@@ -39,14 +46,62 @@ describe('Trips :: Get all', function (): void {
             `)
         })
 
-        it('should return list', function (): void {
-            assert.equal(true, true)
+        const schema = {
+            type: 'object',
+            required: ['total', 'items'],
+            properties: {
+                total: {
+                    type: 'integer',
+                    minimum: 0
+                },
+                items: {
+                    type: 'array',
+                    items: {
+                        type: 'object',
+                        required: ['id', 'title', 'description', 'createdAt', 'updatedAt'],
+                        properties: {
+                            id: {
+                                type: 'string',
+                                format: 'uuid'
+                            },
+                            title: {
+                                type: ['string', 'null'],
+                                maxLength: 255
+                            },
+                            description: {
+                                type: ['string', 'null']
+                            },
+                            createdAt: {
+                                format: 'date-time'
+                            },
+                            updatedAt: {
+                                format: 'date-time'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        it('should return list', async function (): Promise<void> {
+            const res = await server.hapi.inject({
+                method: 'get',
+                url: '/trips'
+            })
+
+            assert.jsonSchema(res.result, schema)
         })
     })
 
+    // ########################################### ###########################################
+
     describe('getAll', function (): void {})
 
+    // ########################################### ###########################################
+
     describe('update', function (): void {})
+
+    // ########################################### ###########################################
 
     describe('destroy', function (): void {})
 })
