51a4dd7c575d5363bbaa216e7e7171ac38ba7677
Vyacheslav Kostenko <namielusi@gmail.com>
2019-06-29 19:23:56 +0400

    Add tests for server

---
+++ test/src/Server.spec.ts
@@ -0,0 +1,22 @@
+import { assert } from 'chai'
+import config from 'config'
+
+import { createServer, prepareServer, stopServer } from '@test/utils/server'
+
+describe('Trips routes', function (): void {
+    const server = createServer()
+    
+    beforeEach(async function (): Promise<void> {
+        await prepareServer(server)
+    })
+    
+    afterEach(async function (): Promise<void> {
+        await stopServer(server)
+    })
+
+    describe('Server', function (): void {
+        it('should run on correct port', function (): void {
+            assert.equal(server.hapi.info.port, config.get('server.port'))
+        })
+    })
+})
